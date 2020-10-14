9ff812f1aefbe91392d2e8b6c279302801abf0cf
jhwang04 <justin@goliath.org>
2020-10-14 11:47:09 -0700

    initial commit

---
+++ Pacman/src/jhwang04/pacman/PacmanApplet.java
@@ -0,0 +1,13 @@
+package jhwang04.pacman;
+
+import processing.core.PApplet;
+
+public class PacmanApplet extends PApplet {
+	public void setup() {
+		size(800, 800);
+	}
+	
+	public void draw() {
+		
+	}
+}
